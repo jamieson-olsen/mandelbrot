f710546b23964cb5162bd1ee429eb9c01610eea7
Jamieson Olsen <jamieson@fnal.gov>
2024-01-23 16:41:41 -0600

    first commit

---
+++ mandelbrot.py
@@ -0,0 +1,30 @@
+import PIL.Image as img
+import numpy as np
+
+xmin = -2.0 
+xmax = 1.0
+ymin = -1.0 
+ymax = 1.0
+xpix = 600
+ypix = 400
+maxiter = 40
+
+pict = img.new( mode="RGB", size=(xpix, ypix) )
+z = np.zeros( (xpix,ypix), dtype=complex )
+
+for x in range(xpix): 
+    for y in range(ypix):
+        z[x][y] = complex( (xmax-xmin)/xpix*x+xmin, (ymax-ymin)/ypix*y+ymin )
+
+for x in range(xpix):  
+    for y in range(ypix):
+        z0 = z[x][y]
+        for i in range(maxiter):
+            zn = z[x][y] ** 2 + z0
+            if ( abs(zn) > 2.0 ):
+                pict.putpixel( (x,y), (255,i*6+15,0) )
+                break
+            else:
+                z[x][y] = zn
+                       
+pict.show()
