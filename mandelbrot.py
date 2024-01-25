3e1a7b0feadb725071ddee419b4c10162cba00bc
Jamieson Olsen <jamieson@fnal.gov>
2024-01-25 17:24:44 -0600

    grayscale 256 version max interations 50

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,15 +1,12 @@
 import PIL.Image as img
 import numpy as np
 
-xmin = -2.0 
-xmax = 1.0
-ymin = -1.0 
-ymax = 1.0
-xpix = 600
-ypix = 400
-maxiter = 255
+xmin, xmax = -2.0, 1.0
+ymin, ymax = -1.0, 1.0
+xpix, ypix = 600, 400
+maxiter = 50
 
-pict = img.new( mode="RGB", size=(xpix, ypix) )
+p = img.new( mode="L", size=(xpix, ypix) )
 c = np.zeros( (xpix,ypix), dtype=complex )
 
 for x in range(xpix): 
@@ -22,7 +19,7 @@ for x in range(xpix):
         for i in range(maxiter):
             z = z ** 2 + c[x][y]
             if ( abs(z) > 2.0 ):
-                pict.putpixel( (x,y), (255-i,0,i) )
+                p.putpixel( (x,y), i*4+25 )
                 break
                        
-pict.show()
+p.show()
